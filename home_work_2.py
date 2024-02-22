92b20cc0804b6a29bdbce3f84e0295603bb2692b
RaccoonSib <158429378+RaccoonSib@users.noreply.github.com>
2024-02-23 00:38:24 +0700

    home_work_2.py

--- home_work_2.py
+++ home_work_2.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 lr = 0.01
 w1 = 0
 w0 = 0
@@ -6,8 +8,16 @@ y = [2, 6, 14]
 
 for i in range(len(X)):
     prediction = w1 * X[i] + w0
-    w1 += 2 * lr * X[i] * (y[i] - prediction)m
+    w1 += 2 * lr * X[i] * (y[i] - prediction)
     w0 += 2 * lr * (y[i] - prediction)
 
 print(f"w0 = {w0}, w1 = {w1}")
-  
+
+# Изобразим точки
+
+plt.scatter(X, y)
+plt.plot(X, [w1 * x + w0 for x in X], color = 'red')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.grid()
+plt.show()
