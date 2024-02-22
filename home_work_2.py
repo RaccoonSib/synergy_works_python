b9c745a0a4fda84e9498dafa85b4ad6cd5a929ff
RaccoonSib <158429378+RaccoonSib@users.noreply.github.com>
2024-02-23 00:28:17 +0700

    Update home_work_2.py

--- home_work_2.py
+++ home_work_2.py
@@ -1,1 +1,13 @@
+lr = 0.01
+w1 = 0
+w0 = 0
+X = [1, 3, 7]
+y = [2, 6, 14]
 
+for i in range(len(X)):
+    prediction = w1 * X[i] + w0
+    w1 += 2 * lr * X[i] * (y[i] - prediction)m
+    w0 += 2 * lr * (y[i] - prediction)
+
+print(f"w0 = {w0}, w1 = {w1}")
+  
